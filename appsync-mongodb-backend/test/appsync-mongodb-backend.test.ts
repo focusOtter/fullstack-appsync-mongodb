7643d812bc7e52ce05be8dca2fc33af3f07c3267
Michael Liendo <mtliendo@gmail.com>
2022-12-15 16:15:07 -0600

    init repos

---
+++ appsync-mongodb-backend/test/appsync-mongodb-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AppsyncMongodbBackend from '../lib/appsync-mongodb-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/appsync-mongodb-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AppsyncMongodbBackend.AppsyncMongodbBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
